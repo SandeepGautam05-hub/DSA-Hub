79901e5b7bc1deaa98f67d09edc9b3fe12993161
Sandeep Gautam <sandeepgautam2115@gmail.com>
2026-07-31 12:51:57 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (49.24%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    // Sandeep Gautam
+    public int missingNumber(int[] nums) {
+        int xor = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= i ^ nums[i];
+        }
+
+        return xor;
+    }
+}
