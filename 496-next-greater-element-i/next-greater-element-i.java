a14578cef318e2669cb5d3eeb72846e64fc90c27
Sandeep Gautam <sandeepgautam2115@gmail.com>
2026-09-08 01:09:48 +0530

    Time: 4 ms (73.99%) | Memory: 45.4 MB (21.89%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,27 +1,27 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
         Stack<Integer> st = new Stack<>();
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        // Find next greater element for every element in nums2
         for (int num : nums2) {
 
+            // Maintain decreasing monotonic stack
             while (!st.isEmpty() && num > st.peek()) {
-                map.put(st.pop(), num);
+
+                int element = st.pop();
+
+                map.put(element, num);
             }
 
             st.push(num);
         }
 
-        // Elements remaining in stack don't have a greater element
+        // Remaining elements have no greater element
         while (!st.isEmpty()) {
             map.put(st.pop(), -1);
         }
 
-        // Build answer for nums1
         int[] ans = new int[nums1.length];
 
         for (int i = 0; i < nums1.length; i++) {
